9030b39aa8ddcf14bf71b3ef7cb1dda84ca9fc87
Joepackins Owino <88088880+Joedgan12@users.noreply.github.com>
2023-12-16 00:45:27 +0300

    Update exec_funcs.c

--- exec_funcs.c
+++ exec_funcs.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * execute_fork - Create a child process to execute a command
- * @c_path: The command to execute
- * @args: The arguments along with the command to be executed
- * @av: 1d array of the command line arguments given when the program is run
+ * execute_fork - Create a process to execute a command
+ * @c_path: command to execute
+ * @args: The arguments to be executed
+ * @av: array of the command line arguments given when the program is run
  * @ev: Environment variables
- * @cmd_cnt: The current command number
+ * @cmd_cnt: command number
  * @status: Pointer at the exit status of the last executed command
- * @commands: The commands given to be freed
+ * @commands: commands given to be set free
  *
  * Return: void
  */
@@ -44,7 +44,7 @@ void execute_fork(char *c_path, char **args, char **av, char **ev, int cmd_cnt,
 
 
 /**
- * exec_line_commands - Execute commands received from stdin
+ * exec_line_commands - Execute commands gotten from stdin
  * @commands: The commands to be executed
  * @av: Argument vector
  * @ev: Environment vector
@@ -56,24 +56,24 @@ void execute_fork(char *c_path, char **args, char **av, char **ev, int cmd_cnt,
 void exec_line_commands(char **commands, char **av, char **ev, int cmd_cnt,
 		    int *status)
 {
-	int i = 0;
+	int m = 0;
 	char *c_path;
 
-	while (commands && commands[i])
+	while (commands && commands[m])
 	{
-		char **args = _strtok(commands[i], " \t");
+		char **args = _strtok(commands[m], " \t");
 
 		args = replace_variables(args, *status);
 		if (args[0] == NULL)
 		{
-			i++;
+			m++;
 			free_arr(args);
 			continue;
 		}
 		if (!_strcmp(args[0], "exit"))
 		{
 			free_arr(commands);
-			exit_program(args, i, cmd_cnt, status);
+			exit_program(args, m, cmd_cnt, status);
 		}
 		if (_strchr(args[0], '/') == NULL)
 		{
@@ -81,27 +81,27 @@ void exec_line_commands(char **commands, char **av, char **ev, int cmd_cnt,
 			{
 				execute_builtin(args, av, cmd_cnt);
 				free_arr(args);
-				i++;
+				m++;
 				continue;
 			}
 			c_path = get_path(args[0]);
 		}
 		else
 			c_path = args[0];
-		if (is_path_null(c_path, status, &i, args, av, cmd_cnt))
+		if (is_path_null(c_path, status, &m, args, av, cmd_cnt))
 			continue;
 		execute_fork(c_path, args, av, ev, cmd_cnt, status, commands);
 		free_if_malloced(c_path, args[0]);
 		free_arr(args);
-		i++;
+		m++;
 	}
 }
 
 /**
  * is_path_null - Checks if extracted path of a command is NULL and performs
- * appropriate action if so
- * @c_path: The extracted command path
- * @status: Pointer to the status of the last executed command
+ * appropriate action if true
+ * @c_path: extracted command path
+ * @status: Pointer to the status of last executed command
  * @iter: Pointer to the iterator of the calling function
  * @args: Arguments given from the user
  * @av: Argument vector given to name which holds the name of the program
